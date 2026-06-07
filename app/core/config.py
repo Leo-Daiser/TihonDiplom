2311f0a9702b986c52b04a641c4730d4fb944311
Nikita Ivanisev <130082652+Leo-Daiser@users.noreply.github.com>
2026-06-07 23:36:46 +0300

    Add JWT settings

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,9 @@ class Settings(BaseSettings):
     app_env: str = "local"
     app_debug: bool = True
     database_url: str
+    secret_key: str
+    access_token_expire_minutes: int = 1440
+    jwt_algorithm: str = "HS256"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
